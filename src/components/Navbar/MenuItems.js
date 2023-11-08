a7f3457cf547e946325a15cfd68eaf0ead7bceb6
Devdeep0109 <devdeepgoyal@gmail.com>
2023-11-08 11:05:32 +0530

    completed

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const menuItems =[
+    {
+        title: 'Home',
+        url :'#',
+        cName : 'nav-links'
+    },
+    {
+        title: 'Services',
+        url :'#',
+        cName : 'nav-links'
+    },
+    {
+        title: 'Product',
+        url :'#',
+        cName : 'nav-links'
+    },
+    {
+        title: 'About Us',
+        url :'#',
+        cName : 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url :'#',
+        cName : 'nav-links-mobile'
+    },
+
+]
